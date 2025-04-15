80ed40b74b1dac4689c9d95bdb88d0315ad02335
Leonardo Rodrigues do Nascimento Filho <leronasfilho@gmail.com>
2025-04-15 14:34:40 -0300

    mudanca na content section

--- components/ContentSection/index.js
+++ components/ContentSection/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { prismTomorrow } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 const CodeBlock = {
   code({ node, inline, className, children, ...props }) {
     const match = /language-(\w+)/.exec(className || "");
     return !inline && match ? (
       <SyntaxHighlighter
-        style={dracula}
+        style={prismTomorrow}
         language={match[1]}
         PreTag="div"
         {...props}
